405d0304cad3d9f50f6a684a8e0b849516b1c1b4
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-02-17 00:24:18 +0300

    Исправление ошибок

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,39 +1,40 @@
 window.addEventListener("DOMContentLoaded", () => {
-  var mySwiper = new Swiper(".swiper-container", {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 30,
+  if (document.body.contains(document.querySelector(".swiper-container")))
+    var mySwiper = new Swiper(".swiper-container", {
+      // Optional parameters
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 30,
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination"
-    },
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination"
+      },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar"
-    }
-  });
-});
-
-const anchors = document.querySelectorAll('a[href*="#"]');
-
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-
-    const blockID = anchor.getAttribute("href").substr(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
+      // And if we need scrollbar
+      scrollbar: {
+        el: ".swiper-scrollbar"
+      }
     });
-  });
-}
+
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    });
+  }
+});
