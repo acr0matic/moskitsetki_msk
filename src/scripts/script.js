33f018fe0ec9b1bd4184d84de58f3ac4ccb510fa
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-02-18 18:17:13 +0300

    Сделана галерея и поправлены другие страницы

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -23,11 +23,32 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-
   const callbackButtons = document.querySelectorAll("[data-modal-callback]");
   for (const button of callbackButtons) {
     button.addEventListener("click", () => {
       MicroModal.show("modal-callback"); // [1]
     });
   }
+
+  var modal = document.getElementById("gallery-modal");
+  var images = document.getElementsByClassName("gallery-item");
+  var modalImg = document.getElementById("gallery-modal-image");
+
+  for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    img.onclick = function() {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+    };
+  }
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  modal.onclick = function(e) {
+    if (e.target != modalImg) modal.style.display = "none";
+  };
 });
