0a31bd1d7024e360ce795186240bb9daa8c2ac8f
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-03-23 14:14:04 +0300

    Исправление навигации

--- src/scripts/smoothscroll.js
+++ src/scripts/smoothscroll.js
@@ -1,10 +1,11 @@
 const anchors = document.querySelectorAll('a[href*="#"]');
+const anchorChange = document.getElementById("anchor-change");
 const anchorButtons = document.querySelectorAll("[data-anchor-button]");
 
 for (let button of anchorButtons) {
   button.addEventListener("click", function(e) {
     e.preventDefault();
-    const blockID = button.getAttribute("data-anchor-button").substr(1);
+    const blockID = button.getAttribute("data-anchor-button");
 
     SmoothScroll(blockID);
   });
@@ -13,19 +14,23 @@ for (let button of anchorButtons) {
 for (let anchor of anchors) {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
-    const blockID = anchor.getAttribute("href").substr(1);
+    const blockID = anchor.getAttribute("href");
 
     SmoothScroll(blockID);
   });
 }
 
 function SmoothScroll(blockID) {
-  var section = document.getElementById(blockID);
+  var section = document.getElementById(blockID.substr(1));
 
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
+  if (section)
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+
+  else
+    window.open(blockID, "_self");
 
   closeMobileMenu();
 }
@@ -43,6 +48,15 @@ for (const button of curtainsScrollButtons) {
   button.addEventListener("click", () => {
     SmoothScroll("curtains");
   });
+}
+
+CheckViewport();
+
+function CheckViewport() {
+  var media = window.matchMedia("(max-width: 768px)");
+
+  if (media.matches)
+    anchorChange.setAttribute("href", "production.html#curtains")
 }
 
 var logoElements = document.querySelectorAll(".logo");
